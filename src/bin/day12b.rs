bb1ca94384c816ab3aa222ca9c4a8916e3666e37
Martin Huschenbett <martin.huschenbett@posteo.me>
2020-12-04 23:27:35 +0100

    Solutions for day 12

---
+++ src/bin/day12b.rs
@@ -0,0 +1,29 @@
+use serde_json::Value;
+use std::fs::File;
+
+type Error = Box<dyn std::error::Error>;
+
+fn main() -> Result<(), Error> {
+    let reader = File::open("inputs/day12.json")?;
+    let value: Value = serde_json::from_reader(reader)?;
+    let mut queue = vec![value];
+    let mut sum = 0;
+    while let Some(value) = queue.pop() {
+        match value {
+            Value::Null | Value::Bool(_) | Value::String(_) => {}
+            Value::Number(n) => {
+                if let Some(n) = n.as_i64() {
+                    sum += n;
+                }
+            }
+            Value::Array(vec) => queue.extend(vec),
+            Value::Object(obj) => {
+                if !obj.values().any(|x| x.as_str() == Some("red")) {
+                    queue.extend(obj.into_iter().map(|x| x.1));
+                }
+            }
+        }
+    }
+    println!("The sum of all numbers is {}", sum);
+    Ok(())
+}
